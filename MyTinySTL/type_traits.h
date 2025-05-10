7545f91803f8843e34276418452d914e92851420
xiaochaobing <153789133+xiaochaobing@users.noreply.github.com>
2025-05-10 11:12:59 +0800

    Update type_traits.h

--- MyTinySTL/type_traits.h
+++ MyTinySTL/type_traits.h
@@ -12,11 +12,13 @@ namespace mystl
 // helper struct
 
 template <class T, T v>
+//编译器封装常量
 struct m_integral_constant
 {
   static constexpr T value = v;
 };
 
+//bool特化
 template <bool b>
 using m_bool_constant = m_integral_constant<bool, b>;
 
@@ -28,14 +30,14 @@ typedef m_bool_constant<false> m_false_type;
 
 // is_pair
 
-// --- forward declaration begin
+// --- forward declaration begin前向声明：这个文件中没有具体实例，告诉编译器有这东西，减少文件的依赖，但不能使用这个类的示例，只能使用它的指针
 template <class T1, class T2>
 struct pair;
-// --- forward declaration end
-
+// 主模版
 template <class T>
 struct is_pair : mystl::m_false_type {};
 
+//特化
 template <class T1, class T2>
 struct is_pair<mystl::pair<T1, T2>> : mystl::m_true_type {};
 
